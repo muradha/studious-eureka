90c617565fed233eb921486c2b171e105e4a1a07
muradha <mhdrizki098@gmail.com>
2023-08-29 17:43:38 +0700

    logout user api

--- test/user.test.js
+++ test/user.test.js
@@ -158,3 +158,32 @@ describe("PATCH /api/users/current", function () {
   })
 
 });
+
+
+describe("DELETE /api/users/logout", () => {
+
+  beforeEach(async () => {
+    await createTestUser();
+  });
+
+  afterEach(async () => {
+    await removeTestUser();
+  })
+
+  it('should can logout', async () => {
+    const result = await supertest(web).delete('/api/users/logout').set("Authorization", "test");
+
+    expect(result.status).toBe(200);
+    expect(result.body.data).toBe("OK");
+
+    const user = await getTestUser();
+
+    expect(user.token).toBeNull();
+  })
+
+  it('should reject logout if token is invalid', async () => {
+    const result = await supertest(web).delete('/api/users/logout').set("Authorization", "salah");
+
+    expect(result.status).toBe(401);
+  })
+})
