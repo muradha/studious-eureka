7322ce402708fd716c0219bc8d800c03cefd8296
muradha <mhdrizki098@gmail.com>
2023-09-03 12:58:12 +0700

    list address api

--- src/service/address-service.js
+++ src/service/address-service.js
@@ -129,9 +129,28 @@ const remove = async(user, contactId, addressId) => {
     });
 }
 
+const list = async(user, contactId) => {
+    contactId = await checkContactMustExist(user, contactId);
+
+    return prismaClient.address.findMany({
+        where:{
+            contact_id: contactId
+        },
+        select:{
+            id: true,
+            street: true,
+            city: true,
+            province: true,
+            postal_code: true,
+            country: true
+        }
+    })
+}
+
 export default {
     create,
     get,
     update,
-    remove
+    remove,
+    list
 }
